5572d0f73ec2782cd19a17e6a7198d1dca589349
keshav <91942229+keshav1010@users.noreply.github.com>
2026-06-02 15:20:48 +0530

    Add ngrok skip warning

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,7 +6,8 @@ const api = axios.create({
         import.meta.env.VITE_API_BASE_URL,
 
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true"
     }
 });
 
